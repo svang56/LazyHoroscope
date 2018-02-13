2777eae8dda3cf9c2bf9355ad5e24bd47b672cba
svang56 <svang56@mail.csuchico.edu>
2018-02-12 17:53:30 -0800

    First Last name Input fortune Algorithm

--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@ int main()
 {
   //declare variables
     string first, last;
-
+    string lucky;
   //get user input
   cout<< "Please enter your first and then last name."<<endl;
     cin>>first;
     cin>>last;
+  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..."<<endl;   
   //tell fortune
-
+  lucky = first + last;
+    cout<<"Your lucky nmber is: "<<lucky.length()<< "."<<endl;
   return 0;
 }
