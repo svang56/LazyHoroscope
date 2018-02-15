6689b012f81c17591c57dac7252053a13ac7020a
svang56 <svang56@mail.csuchico.edu>
2018-02-14 17:43:30 -0800

    First and Last Name Input Fortune Algorithm

--- main.cpp
+++ main.cpp
@@ -12,30 +12,31 @@ int main()
 
   //get user input
 
-  cout<< "Please enter your first and then last name."<<endl;
+  cout<< "What is your first name?"<<endl;
     cin>>first;
+  cout<< "What is your last name?" << endl;
     cin>>last;
   cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..."<<endl;   
   //tell fortune
 
   lucky = first.length();
-    cout<<"Your lucky number is: "<<lucky<< "."<<endl;
+    cout<<"your lucky number is "<<lucky<<endl;
     
     if( first[0] == 'a' || first[0] == 'e' || first[0] == 'i' || first[0] == 'o' || first[0] == 'u' || first[0] == 'A' || first[0] == 'E' || first[0] == 'I' || first[0] == 'O' || first[0] == 'U')
     {
-    cout<<"You are destined to be famous!"<<endl;
+    cout<<"you are destined to be famous!"<<endl;
     }
     else
     {
-    cout<<"You should keep a low profile."<<endl;
+    cout<<"you should keep a low profile."<<endl;
     }
 
     int endvow = last.length();
 
     if( last[endvow - 1] == 'a' || last[endvow - 1] == 'e' || last[endvow - 1] == 'i' || last[endvow - 1] == 'o' || last[endvow - 1] == 'u' || last[endvow -1] == 'A' || last[endvow - 1] == 'E' || last[endvow - 1] == 'O' || last[endvow - 1] == 'O' || last[endvow - 1] == 'U')
     { 
-    cout << "You have already met your true love." <<endl;
+    cout << "you have already met your true love." <<endl;
     }
-    cout << "Have a good day!"<<endl;
+    cout << "have a good day!"<<endl;
   return 0;
 }
