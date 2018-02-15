075ed14499c8fe1342072ae178590f85844db82d
svang56 <svang56@mail.csuchico.edu>
2018-02-14 17:35:33 -0800

    First and Last Name Input Fortune Algorithm

--- main.cpp
+++ main.cpp
@@ -20,6 +20,22 @@ int main()
 
   lucky = first.length();
     cout<<"Your lucky number is: "<<lucky<< "."<<endl;
+    
+    if( first[0] == 'a' || first[0] == 'e' || first[0] == 'i' || first[0] == 'o' || first[0] == 'u' || first[0] == 'A' || first[0] == 'E' || first[0] == 'I' || first[0] == 'O' || first[0] == 'U')
+    {
+    cout<<"You are destined to be famous!"<<endl;
+    }
+    else
+    {
+    cout<<"You should keep a low profile."<<endl;
+    }
 
+    int endvow = last.length();
+
+    if( last[endvow - 1] == 'a' || last[endvow - 1] == 'e' || last[endvow - 1] == 'i' || last[endvow - 1] == 'o' || last[endvow - 1] == 'u' || last[endvow -1] == 'A' || last[endvow - 1] == 'E' || last[endvow - 1] == 'O' || last[endvow - 1] == 'O' || last[endvow - 1] == 'U')
+    { 
+    cout << "You have already met your true love." <<endl;
+    }
+    cout << "Have a good day!"<<endl;
   return 0;
 }
